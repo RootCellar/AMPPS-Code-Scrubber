5d365a0483a4e3ecbee91a27b4e52a6055aa1e10
RootCellar <RootCellar9877@gmail.com>
2025-02-17 11:34:20 -0900

    create unit-tests.c, build executable using makefile, add executable to gitignore

---
+++ memory-voting-prototype/unit-tests.c
@@ -0,0 +1,25 @@
+
+/*
+ *
+ * Author: Darian Marvel
+ * 2/17/2025
+ *
+ * Unit tests for AMPPS memory correction algorithms
+ *
+*/
+
+
+// Includes
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "memory_correction.h"
+#include "testing.h"
+
+// Main
+
+int main(int argc, char** argv) {
+  return 0;
+}
