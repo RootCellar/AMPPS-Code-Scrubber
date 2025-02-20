f9c20ad2f43edf8f294601089d2450365a5c14f3
RootCellar <RootCellar9877@gmail.com>
2025-02-19 22:31:41 -0900

    test: 5 copies with bit flips are corrected

--- memory-voting-prototype/unit_tests.c
+++ memory-voting-prototype/unit_tests.c
@@ -58,6 +58,27 @@ int main(int argc, char** argv) {
     TEST(collection.data_copies[2][0] == original_data, "the third copy is fully corrected");
   }
 
+  TEST_NAME("5 copies with bit flips are corrected");
+  {
+    struct data_copies_collection collection = create_data_copy_collection(5, 1);
+
+    char original_data = 0b10101010;
+
+    collection.data_copies[0][0] = 0b10001111;
+    collection.data_copies[1][0] = 0b11111000;
+    collection.data_copies[2][0] = 0b00101010;
+    collection.data_copies[3][0] = 0b01100110;
+    collection.data_copies[4][0] = 0b10110010;
+
+    int fixes = correct_bits(collection.data_copies, 5, 0);
+    TEST(fixes == 13, "correct_bits reports making 13 fixes");
+    TEST(collection.data_copies[0][0] == original_data, "the first copy is fully corrected");
+    TEST(collection.data_copies[1][0] == original_data, "the second copy is fully corrected");
+    TEST(collection.data_copies[2][0] == original_data, "the third copy is fully corrected");
+    TEST(collection.data_copies[3][0] == original_data, "the fourth copy is fully corrected");
+    TEST(collection.data_copies[4][0] == original_data, "the fifth copy is fully corrected");
+  }
+
   show_test_results();
 
   return 0;
