2f7217d46a9b71d8eb11ea3cbaf7fe1443feb117
RootCellar <RootCellar9877@gmail.com>
2025-02-23 14:16:01 -0900

    implement randomly flipped bits demo and add titles

--- memory-voting-prototype/demo.c
+++ memory-voting-prototype/demo.c
@@ -5,6 +5,7 @@
 #include "memory_correction.h"
 #include "terminal_color.h"
 #include "testing.h"
+#include "time.h"
 #include "util.h"
 
 int main(int argc, char** argv) {
@@ -17,14 +18,28 @@ int main(int argc, char** argv) {
     exit(EXIT_FAILURE);
   }
 
+  srand(get_time().tv_nsec);
+
   struct data_copies_collection collection = create_data_copy_collection(num_copies, len);
 
   char original_byte = 0b11001100;
   for(int i = 0; i < len; i++) collection.original_data[i] = original_byte;
   copy_original_to_copies(collection.original_data, collection.data_copies, collection.num_copies, collection.data_size);
 
+  printf("%s\n Whole Flipped Bytes %s\n", ANSI_COLOR_BRIGHT_YELLOW, ANSI_COLOR_RESET);
+
   for(size_t i = 0; i < len; i++) {
     collection.data_copies[i % num_copies][i] = ~original_byte;
+  }
+
+  print_correct_errors_demo(num_copies, len, collection);
+
+  printf("%s\n Randomly Flipped Bits %s\n", ANSI_COLOR_BRIGHT_YELLOW, ANSI_COLOR_RESET);
+
+  for(size_t i = 0; i < len; i++) {
+    for(int k = 0; k < BITS_IN_BYTE; k++) {
+      collection.data_copies[rand() % num_copies][i] ^= 1 << k;
+    }
   }
 
   print_correct_errors_demo(num_copies, len, collection);
