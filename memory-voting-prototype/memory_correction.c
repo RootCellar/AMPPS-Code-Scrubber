1afe964defa95116c9e0c3b4bc2ac25fb666fba1
RootCellar <RootCellar9877@gmail.com>
2025-02-23 17:23:04 -0900

    refactor: move variable initializations to top of functions to make porting to MSP easier
    includes other cleanup

--- memory-voting-prototype/memory_correction.c
+++ memory-voting-prototype/memory_correction.c
@@ -6,6 +6,8 @@
 // Performs a bit-level correction of the byte at loc in data_copies
 char correct_bits(char** data_copies, int num_copies, int loc) {
 
+  int i, j, k;
+
   char agreements[num_copies];
 
   char most_agreements;
@@ -13,10 +15,10 @@ char correct_bits(char** data_copies, int num_copies, int loc) {
 
   char corrections = 0;
 
-  for(int i = 0; i < BITS_IN_BYTE; i++) {
+  for(i = 0; i < BITS_IN_BYTE; i++) {
 
     // Reset tracked agreements
-    for(int k = 0; k < num_copies; k++) agreements[k] = 0;
+    for(k = 0; k < num_copies; k++) agreements[k] = 0;
     most_agreements = 0;
     most_agreed_value = 0;
 
@@ -24,15 +26,16 @@ char correct_bits(char** data_copies, int num_copies, int loc) {
 
     // Search forward for data copies that agree
     // and keep track of how many are found
-    for(int k = 0; k < num_copies; k++)
-      for(int j = k+1; j < num_copies; j++) {
+    for(k = 0; k < num_copies; k++) {
+      for(j = k + 1; j < num_copies; j++) {
         if((data_copies[j][loc] & current_bit) == (data_copies[k][loc] & current_bit)) {
           agreements[k]++;
         }
       }
+    }
 
     // Find the most agreed value
-    for(int k = 0; k < num_copies; k++) {
+    for(k = 0; k < num_copies; k++) {
       if(agreements[k] > most_agreements) {
         most_agreements = agreements[k];
         most_agreed_value = data_copies[k][loc] & current_bit;
@@ -40,7 +43,7 @@ char correct_bits(char** data_copies, int num_copies, int loc) {
     }
 
     // Correct data copies to match
-    for(int k = 0; k < num_copies; k++) {
+    for(k = 0; k < num_copies; k++) {
       if((data_copies[k][loc] & current_bit) != most_agreed_value) {
         data_copies[k][loc] ^= current_bit;
         corrections++;
@@ -58,31 +61,30 @@ char correct_bits(char** data_copies, int num_copies, int loc) {
 // at the bit level.
 int correct_errors(char** data_copies, int num_copies, int data_size) {
 
-  if(num_copies < 2) return -1;
-  if(data_size % sizeof(SEEK_TYPE) != 0) return -1;
-
   SEEK_TYPE** data_copies_cast = (SEEK_TYPE**) data_copies;
 
   int corrections = 0;
+  size_t i, j;
+  int k;
 
-  for(size_t i = 0; i < data_size / sizeof(SEEK_TYPE); i++) {
+  if(num_copies < 2) return -1;
+  if(data_size % sizeof(SEEK_TYPE) != 0) return -1;
+
+  for(i = 0; i < data_size / sizeof(SEEK_TYPE); i++) {
 
     SEEK_TYPE value = data_copies_cast[0][i];
 
     // Search forward through the data copies
     // and make sure they all agree with the current byte.
     // If they don't, perform a correction
-    for(int k = 1; k < num_copies; k++) {
+    for(k = 1; k < num_copies; k++) {
       if(data_copies_cast[k][i] != value) {
-
-        for(size_t j = 0; j < sizeof(SEEK_TYPE); j++) {
+        for(j = 0; j < sizeof(SEEK_TYPE); j++) {
           corrections += correct_bits(data_copies, num_copies, i * sizeof(SEEK_TYPE) + j);
         }
-
         k = num_copies;
       }
     }
-
   }
 
   return corrections;
