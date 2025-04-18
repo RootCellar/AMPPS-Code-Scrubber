622c7cf2549f74b79db3c891340e5e88bd6af888
RootCellar <RootCellar9877@gmail.com>
2025-04-18 05:50:37 -0800

    correct header to have correct types

--- memory-voting-prototype/memory_correction.h
+++ memory-voting-prototype/memory_correction.h
@@ -6,7 +6,7 @@
 #define SEEK_TYPE char
 #define BITS_IN_BYTE (8)
 
-char correct_bits(char** data_copies, int num_copies, int loc);
-int correct_errors(char** data_copies, int num_copies, int data_size);
+char correct_bits(char** data_copies, int num_copies, int32_t loc);
+int32_t correct_errors(char** data_copies, int num_copies, int32_t data_size);
 
 #endif
